ae591c99adf4d3300bf436e3280312591b16ee43
Muhammad Mubashir <Muhammad Mubashir@ENGINEER-PC>
2023-01-01 15:44:08 +0500

    Add project files.

---
+++ Mangoe/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Mangoe.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Mangoe.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
